9815a95897303e6f2bce3d36d7de2254f6cc09c8
Yugal Pachpande <pachpande.yugal@gmail.com>
2016-08-04 20:22:37 +0530

    Added argumenr parser python file

---
+++ python/Sys.py
@@ -0,0 +1,12 @@
+import sys, getopt
+myopts, args = getopt.getopt(sys.argv[1:],"i:o:")
+
+
+for o, a in myopts:
+    if o == '-i':
+        arg1=a
+    elif o == '-o':
+        arg2=a
+arg1_to_int = int(arg1)   
+arg2_to_int = int(arg2)   
+print "Addition of ",arg1,"and",arg2,"is",arg1_to_int + arg2_to_int
