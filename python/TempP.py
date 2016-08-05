55fec71b47071dca99ee139c104323ee24291c0c
yograjshisode <yograjshisode@gmail.com>
2016-08-05 11:25:33 +0530

    Python programs

---
+++ python/TempP.py
@@ -0,0 +1,5 @@
+
+dic={"Name":"Abc","Phoneno":["90909090","494949494"]}
+dic['Phoneno']=["121212","3333333","9999999"]
+keys=["name","Phoneno"]
+print dic[keys[1]][1]
---
+++ python/temp.py
@@ -0,0 +1,11 @@
+str=[['MMM', '3M', '100', '74.00', '118.30'],
+ ['MDT', 'Medtronic', '50', '56.10', '48.00'],
+ ['NWAC', 'Northwest Airlines', '100', '67.50', '18.04'],
+ ['SGI', 'Silicon Graphics', '100', '22.25', '2.25']]
+keys=["0","0","0"]
+keys[0]=str[0][1]
+print str[0][1]
+#print keys
+#for i in range(0,len(str)) :
+#    keys[i]=str[i][1]
+#print len(str)
